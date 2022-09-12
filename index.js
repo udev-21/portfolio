edb19b556d6625897199b1ba7829ad90baf0ce33
Sharof Khudayberdiev <udev21@gmail.com>
2022-09-12 21:20:39 +0500

    add active section highlight

--- index.js
+++ index.js
@@ -15,3 +15,34 @@ document.querySelectorAll(".menu-list li a").forEach(elem => {
         menuSection.classList.toggle("d-none");
     });
 });
+
+let recentWorks = document.getElementById("recent-works");
+let aboutMe = document.getElementById("about");
+let contact = document.getElementById("contact");
+
+let links = Array.from(document.querySelectorAll("ul.desktop-only > li > a"));
+let recentWorksSection = links[0];
+let aboutMeSection = links[1];
+let contactSection = links[2];
+
+
+
+window.onscroll = () => {
+    if(recentWorks.getBoundingClientRect().top <= 50 && recentWorks.getBoundingClientRect().top >= -1000){
+        recentWorksSection.classList.add("active-section");
+    }else {
+        recentWorksSection.classList.remove("active-section");
+    }
+
+    if(aboutMe.getBoundingClientRect().top <= 50 && aboutMe.getBoundingClientRect().top >= -325){
+        aboutMeSection.classList.add("active-section");
+    }else {
+        aboutMeSection.classList.remove("active-section");
+    }
+    
+    if(contact.getBoundingClientRect().top <= 350){
+        contactSection.classList.add("active-section");
+    }else {
+        contactSection.classList.remove("active-section");
+    }
+};
