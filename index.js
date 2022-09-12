517b19691b6c6acb4ea7a8a7df7484d46f495784
Sharof Khudayberdiev <udev21@gmail.com>
2022-09-12 20:09:03 +0500

    add link to menu items, used toggle method in js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let menuSection = document.querySelector('#mobile-menu')
 let menu = menuSection.querySelector(".icon-wrapper");
 menu.addEventListener('click', () => {
-    menuSection.classList.add("d-none");
+    menuSection.classList.toggle("d-none");
 });
 
 let hamburgerIcon = document.querySelector("nav.toggle-wrapper .icon-wrapper");
 
 hamburgerIcon.addEventListener('click', () => {
-    menuSection.classList.remove("d-none");
+    menuSection.classList.toggle("d-none");
 });
