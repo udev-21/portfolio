0f3bfe34cd695577eed58b1bb6fa5d34c94e0aec
Sharof Khudayberdiev <udev21@gmail.com>
2022-09-13 14:18:33 +0500

    static projects to dynamic in recent works section

--- index.js
+++ index.js
@@ -46,3 +46,88 @@ window.onscroll = () => {
         contactSection.classList.remove("active-section");
     }
 };
+
+const projects = [
+    {
+        title: "Multi-Post Stories <span class=\"newline\"></span> Gain+Glory",
+        technologies: ['Ruby on Rails', "CSS", "JavaScript", "HTML"],
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+        demoLink: "http://example.com",
+        sourceLink: "http://example.com",
+        imageLink: "/image.jpg",
+    },
+    {
+        title: "Multi-Post Stories <span class=\"newline\"></span> Gain+Glory",
+        technologies: ['Ruby on Rails', "CSS", "JavaScript", "HTML"],
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+        demoLink: "http://example.com",
+        sourceLink: "http://example.com",
+        imageLink: "/image.jpg",
+    },
+    {
+        title: "Multi-Post Stories <span class=\"newline\"></span> Gain+Glory",
+        technologies: ['Ruby on Rails', "CSS", "JavaScript", "HTML"],
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+        demoLink: "http://example.com",
+        sourceLink: "http://example.com",
+        imageLink: "/image.jpg",
+    },
+    {
+        title: "Multi-Post Stories <span class=\"newline\"></span> Gain+Glory",
+        technologies: ['Ruby on Rails', "CSS", "JavaScript", "HTML"],
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+        demoLink: "http://example.com",
+        sourceLink: "http://example.com",
+        imageLink: "/image.jpg",
+    },
+    {
+        title: "Multi-Post Stories <span class=\"newline\"></span> Gain+Glory",
+        technologies: ['Ruby on Rails', "CSS", "JavaScript", "HTML"],
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+        demoLink: "http://example.com",
+        sourceLink: "http://example.com",
+        imageLink: "/image.jpg",
+    },
+    {
+        title: "Multi-Post Stories <span class=\"newline\"></span> Gain+Glory",
+        technologies: ['Ruby on Rails', "CSS", "JavaScript", "HTML"],
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+        demoLink: "http://example.com",
+        sourceLink: "http://example.com",
+        imageLink: "/image.jpg",
+    }
+];
+
+let projectsWrapper = recentWorks.querySelector('.projects');
+
+projects.forEach((project, idx) =>  {
+    let projectCard = document.createElement("div");
+    projectCard.classList.add('project-card');
+    
+    let projectInfo = document.createElement("div");
+    projectInfo.classList.add('project-info');
+    
+    let projectTitle = document.createElement("div");
+    projectTitle.classList.add('project-title');
+    projectTitle.innerHTML = project.title;
+
+    let projectStacks = document.createElement("div");
+    project.technologies.forEach((tech) => {
+        let badge = document.createElement("div");
+        badge.innerText = tech;
+        projectStacks.appendChild(badge);
+    });
+    projectStacks.classList.add('project-stacks');
+
+    let buttonWrapper = document.createElement("div");
+    buttonWrapper.classList.add('button-wrapper');
+    let seeProjectButton = document.createElement("a");
+    seeProjectButton.dataset.id = idx;
+    seeProjectButton.classList.add("button");
+    seeProjectButton.innerText = "See Project";
+    buttonWrapper.appendChild(seeProjectButton);
+
+    projectInfo.append(projectTitle, projectStacks, buttonWrapper);
+    projectCard.appendChild(projectInfo);
+    projectsWrapper.appendChild(projectCard);
+})
